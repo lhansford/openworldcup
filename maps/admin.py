89a9ad693159d6835e5afe0ea732c34d026668d1
Luke Hansford <l.s.hansford@gmail.com>
2013-08-17 16:34:33 +1000

    OWC - non-Django

--- maps/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from maps.models import Nation, Club, Player, Coach
-
-admin.site.register(Nation)
-admin.site.register(Club)
-admin.site.register(Player)
-admin.site.register(Coach)
